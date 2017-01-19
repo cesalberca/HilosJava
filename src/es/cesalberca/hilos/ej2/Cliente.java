2264ea517842b833aafa9cba7f97bca9f1590674
cesalberca <cesalberca@gmail.com>
2017-01-19 21:09:48 +0100

    Supermarket

---
+++ src/es/cesalberca/hilos/ej2/Cliente.java
@@ -0,0 +1,7 @@
+package es.cesalberca.hilos.ej2;
+
+/**
+ * Created by César
+ */
+public class Cliente {
+}
--- src/es/cesalberca/hilos/ej2/SuperMarket.java
+++ src/es/cesalberca/hilos/ej2/SuperMarket.java
@@ -1,7 +1,23 @@
 package es.cesalberca.hilos.ej2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Cesar
  */
 public class SuperMarket {
+    private List<Cliente> clientes;
+    private List<Caja> cajas;
+
+    private int numeroCajas;
+    private int numeroClientes;
+
+    public SuperMarket(int numeroCajas, int numeroClientes) {
+        this.numeroCajas = numeroCajas;
+        this.numeroClientes = numeroClientes;
+
+        clientes = new ArrayList<>();
+        cajas = new ArrayList<>();
+    }
 }
